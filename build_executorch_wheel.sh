f5fb6059ff0b0d926244ddbd4a5d68fcafcc9b5d
kadu-v <kazuonsunday@yahoo.co.jp>
2024-11-01 19:50:55 +0900

    Update: workflow yaml

---
+++ build_executorch_wheel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+(
+    export EXECUTORCH_BUILD_PYBIND=ON
+    export CMAKE_ARGS=" -DEXECUTORCH_BUILD_COREML=ON -DEXECUTORCH_BUILD_MPS=ON -DEXECUTORCH_BUILD_XNNPACK=ON"
+    export CMAKE_BUILD_ARGS=""
+    cd executorch
+    python3 setup.py bdist_wheel \
+        --dist-dir=$SCRIPT_DIR/out-executorch-wheel
+)
+
