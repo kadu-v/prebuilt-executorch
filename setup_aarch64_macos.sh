1203c5e7cf42c993650de23cd75c00c0857243a2
kadu-v <kazuonsunday@yahoo.co.jp>
2024-12-10 11:13:13 +0900

    ✨ : New feature to build devtools binary

--- setup_aarch64_macos.sh
+++ setup_aarch64_macos.sh
@@ -3,3 +3,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install dependencies
 $SCRIPT_DIR/executorch/install_requirements.sh --pybind coreml mps xnnpack
+$SCRIPT_DIR/executorch/backends/apple/coreml/scripts/install_requirements.sh
+$SCRIPT_DIR/executorch/backends/apple/mps/install_requirements.sh
