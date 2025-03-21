4fc1aa55e02e09397b28046045bdfdaaf0d0bda7
kadu-v <kazuonsunday@yahoo.co.jp>
2025-03-21 23:10:50 +0900

    fix: update setup script to include pinned commit option for executorch installation

--- setup_amd64_linux.sh
+++ setup_amd64_linux.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install dependencies
 $SCRIPT_DIR/executorch/install_requirements.sh --use-pt-pinned-commit
-$SCRIPT_DIR/executorch/install_executorch.sh --pybind xnnpack 
+$SCRIPT_DIR/executorch/install_executorch.sh --pybind xnnpack --use-pt-pinned-commit
