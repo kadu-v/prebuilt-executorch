5c5039099369cf847c43c84b3e05e22d66059f4e
kadu-v <kazuonsunday@yahoo.co.jp>
2025-03-21 17:11:36 +0900

    ✨ : Update executorch subproject and add new wheel file for version 0.6.0a0

--- setup_aarch64_macos.sh
+++ setup_aarch64_macos.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install dependencies
-$SCRIPT_DIR/executorch/install_requirements.sh --pybind coreml mps xnnpack
+$SCRIPT_DIR/executorch/install_requirements.sh --use-pt-pinned-commit
+$SCRIPT_DIR/executorch/install_executorch.sh --pybind coreml mps xnnpack 
 $SCRIPT_DIR/executorch/backends/apple/coreml/scripts/install_requirements.sh
 $SCRIPT_DIR/executorch/backends/apple/mps/install_requirements.sh
