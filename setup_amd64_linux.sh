e07e11eef02ae7cdab0c358f7d09cae6e4ad79ac
kadu-v <kazuonsunday@yahoo.co.jp>
2025-03-21 15:01:35 +0000

    fix: update setup script to enhance installation reliability

--- setup_amd64_linux.sh
+++ setup_amd64_linux.sh
@@ -3,7 +3,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Rewrite $SCRIPT_DIR/executorch/install_requirements.sh due to executorch bug
 # Replace torch==2.7.0.{NIGHTLY_VERSION} to torch==2.7.0.dev20250310
-sed -i '' 's/torch==2.7.0\.[0-9]*/torch==2.7.0.dev20250310/g' $SCRIPT_DIR/executorch/install_requirements.sh
+sed -i '' "s/torch==2.7.0\.{NIGHTLY_VERSION}/torch==2.7.0.dev20250310/g" "$SCRIPT_DIR/executorch/install_requirements.py"
+
 
 # Install dependencies
 $SCRIPT_DIR/executorch/install_requirements.sh
