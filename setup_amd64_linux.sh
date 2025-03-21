2f9cf888d3f8eeedd576e2d5e9e1ac8487965904
kadu-v <kazuonsunday@yahoo.co.jp>
2025-03-21 21:06:06 +0900

    fix: remove pinned commit option from executorch installation script

--- setup_amd64_linux.sh
+++ setup_amd64_linux.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install dependencies
-$SCRIPT_DIR/executorch/install_requirements.sh --use-pt-pinned-commit
+$SCRIPT_DIR/executorch/install_requirements.sh
 $SCRIPT_DIR/executorch/install_executorch.sh 
