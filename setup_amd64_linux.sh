50926364aebf6be6ced5c0ec6ea75f34981e0d42
kadu-v <kazuonsunday@yahoo.co.jp>
2025-03-21 22:09:45 +0900

    fix: update executorch installation script to use pybind xnnpack

--- setup_amd64_linux.sh
+++ setup_amd64_linux.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install dependencies
 # $SCRIPT_DIR/executorch/install_requirements.sh
-$SCRIPT_DIR/executorch/install_executorch.sh --use-pt-pinned-commit 
+$SCRIPT_DIR/executorch/install_executorch.sh --pybind xnnpack
